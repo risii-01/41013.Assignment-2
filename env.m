55f46f19232855e6d460efae2bb0081bc675ce48
Risisara Wijesundera <145890502+risii-01@users.noreply.github.com>
2023-11-07 12:14:50 +1100

    Update env.m

--- env.m
+++ env.m
@@ -1,6 +1,12 @@
-% creating a home environment 
-%%
 clc,clf;
+
+hold on;
+
+% workspace = [-2 2 -3 2 0 4];                                       
+% scale = 0.5;        
+% % q = zeros(1,3);                                                  
+% q = [0 0 0 0 0 0];
+
 h = surf([-2,-2;2,2] ...
 ,[2,2;2,2] ...
 ,[0 4; 0 4] ...
@@ -23,11 +29,12 @@ h3 = surf([-2,-2;2,2] ...
 dir = [1 0 0];
 dir2 = [0 1 0];
 
-% rotate(h2,dir,90);
 rotate(h,dir2,90);
 
 table = PlaceObject('newtable2.ply',[-0.5,-1,0.1]);
 shelf = PlaceObject('newshelf9.ply',[1.2,-1.2,0.1]);
+button = PlaceObject('ebutton4.ply',[-1.2,-1.5,0.935]);
+fire = PlaceObject('fireExtinguisher.ply',[1.2,-1.8,0.1]);
 
 
 xlabel('X-axis');
